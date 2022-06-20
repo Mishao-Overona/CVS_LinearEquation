c863aac68adbfdf7e2405f4f77ec1e02161db61a
михаил <morozok.morozhenyy@mail.ru>
2022-06-20 19:40:11 +0300

    feat: test for feature3

--- LW_EquationTest/LinearEquationTest.cs
+++ LW_EquationTest/LinearEquationTest.cs
@@ -122,5 +122,17 @@ namespace LW_EquationTest
 
             Assert.IsFalse(ans);
         }
+
+        [TestMethod]
+        public void LinearEquationTestSolve()
+        {
+            LinearEquation a = new LinearEquation(2, 0, 5);
+
+            float ans = 0;
+            a.Solve(out ans);
+            float trueans = (0F - 5F) / 2F;
+            bool t = ans.CompareTo(trueans) == 0;
+            Assert.IsTrue(t);
+        }
     }
 }

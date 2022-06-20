29655daa849935bd9e2dc51ee3e37e596fb3afd3
михаил <morozok.morozhenyy@mail.ru>
2022-06-20 21:58:00 +0300

    fix: tests has changed in features

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace LW_Equation
 {
     public class LinearEquation
     {
         List<float> coefficients;
         public int Size => coefficients.Count;
+        /// <summary>
+        /// Конструирует уравнение вида coefficients[0]x + ... + coefficients[N-2]y + (aN)z + b = 0
+        /// </summary>
+        /// <param name="b">Свободный член</param>
+        /// <param name="aN">Последний коэффициент</param>
+        /// <param name="coefficients">Остальные коэффициенты</param>
         public LinearEquation(params float[] coefficients)
         {
             this.coefficients = new List<float>();
@@ -20,6 +25,9 @@ namespace LW_Equation
             this.coefficients = new List<float>();
             this.coefficients = coefficients;
         }
+        /// <summary>
+        /// Суммирует свободный член first с second
+        /// </summary>
         static public LinearEquation operator +(LinearEquation first, float second)
         {
             LinearEquation equation = first;
@@ -27,6 +35,9 @@ namespace LW_Equation
 
             return equation;
         }
+        /// <summary>
+        /// Вычитает second из свободного члена first
+        /// </summary>
         static public LinearEquation operator -(LinearEquation first, float second)
         {
             LinearEquation equation = first;
@@ -59,6 +70,125 @@ namespace LW_Equation
         public float this[int i]
         {
             get { return this.coefficients[i]; }
+        }
+
+        static public LinearEquation operator +(LinearEquation a, LinearEquation b)
+        {
+            List<float> first = a.Size > b.Size ? a.coefficients : b.coefficients;
+            List<float> second = a.Size <= b.Size ? a.coefficients : b.coefficients;
+            for (int i = second.Count - 1, j = first.Count - 1; i > 0; i--, j--)
+            {
+                first[j] += second[i];
+            }
+            first[0] += second[0];
+            return new LinearEquation(first);
+        }
+        static public LinearEquation operator -(LinearEquation a, LinearEquation b)
+        {
+            List<float> first = a.Size > b.Size ? a.coefficients : b.coefficients;
+            List<float> second = a.Size <= b.Size ? a.coefficients : b.coefficients;
+            for (int i = second.Count - 1, j = first.Count - 1; i > 0; i--, j--)
+            {
+                first[j] -= second[i];
+            }
+            first[0] -= second[0];
+            return new LinearEquation(first);
+        }
+
+        static public bool operator true(LinearEquation eq)
+        {
+            int count = 0;
+            for (int i = 0; i < eq.Size; i++)
+            {
+                if (eq[i] == 0)
+                    count++;
+            }
+            if (count == eq.Size - 2 && eq[eq.Size - 1] != 0)
+                return true;
+            else
+                return false;
+        }
+        static public bool operator false(LinearEquation eq)
+        {
+            int count = 0;
+            for (int i = 0; i < eq.Size; i++)
+            {
+                if (eq[i] == 0)
+                    count++;
+            }
+            if (count == eq.Size - 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Solve(out float ans)
+        {
+            ans = 0;
+            int counter = 0;
+            int ind = -1;
+            for (int i = Size - 1; i >= 0; i--)
+            {
+                if (this[i] == 0) counter++;
+                else ind = i;
+            }
+            if (counter == Size - 2 && this[Size - 1] != 0)
+            {
+                ans = (0 - this[Size - 1]) / (this[ind]);
+                return true;
+            }
+            return false;
+        }
+        public override String ToString()
+        {
+            String ans = "";
+            for (int i = 0; i < this.Size - 1; i++)
+            {
+                ans += this[i].ToString();
+                ans += ",";
+            }
+            ans += this[this.Size - 1].ToString();
+            return ans;
+        }
+
+        public LinearEquation(bool t, int size)
+        {
+            Random rng = new Random();
+            this.coefficients = new List<float>();
+            for (int i = 0; i < size; i++)
+                coefficients.Add((float)rng.NextDouble() * 100);
+        }
+        public LinearEquation(bool t, int size, float a)
+        {
+            this.coefficients = new List<float>();
+            for (int i = 0; i < size; i++)
+                coefficients.Add(a);
+        }
+        static public LinearEquation operator *(LinearEquation e, float f)
+        {
+            for (int i = 0; i < e.Size; i++)
+            {
+                e.coefficients[i] *= f;
+            }
+            return e;
+        }
+        static public LinearEquation operator -(LinearEquation a)
+        {
+            for (int i = 0; i < a.Size; i++)
+            {
+                a.coefficients[i] *= -1;
+            }
+            return a;
+        }
+        public List<double> ToDouble()
+        {
+            return new List<double>();
+            List<double> result = new List<double>();
+            foreach (float c in coefficients)
+            {
+                result.Add((double)c);
+            }
+            return result;
         }
     }
 }
--- LW_EquationTest/LinearEquationTest.cs
+++ LW_EquationTest/LinearEquationTest.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LW_Equation;
 using System;
-
+using System.Collections.Generic;
 namespace LW_EquationTest
 {
     [TestClass]
@@ -10,11 +10,17 @@ namespace LW_EquationTest
         [TestMethod]
         public void LinearEquationTestEquals()
         {
-            LinearEquation a = new LinearEquation(1, 3);
+            LinearEquation a = new LinearEquation(1, 2);
             LinearEquation b = new LinearEquation(1, 2);
-
             bool result = a == b;
-
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void LinearEquationTestEquals2()
+        {
+            LinearEquation a = new LinearEquation(1, 2);
+            LinearEquation b = new LinearEquation(1, 3);
+            bool result = a == b;
             Assert.IsFalse(result);
         }
         [TestMethod]
@@ -22,9 +28,7 @@ namespace LW_EquationTest
         {
             LinearEquation a = new LinearEquation(1, 2, 3);
             LinearEquation b = new LinearEquation(1, 2);
-
             bool result = a == b;
-
             Assert.IsFalse(result);
         }
         [TestMethod]
@@ -32,9 +36,7 @@ namespace LW_EquationTest
         {
             LinearEquation a = new LinearEquation(1, 3);
             LinearEquation b = new LinearEquation(1, 2);
-
             bool result = a != b;
-
             Assert.IsTrue(result);
         }
         [TestMethod]
@@ -42,56 +44,217 @@ namespace LW_EquationTest
         {
             LinearEquation a = new LinearEquation(1, 2, 3);
             LinearEquation b = new LinearEquation(1, 2);
-
             bool result = a != b;
-
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void LinearEquationTestIndexer()
         {
             LinearEquation a = new LinearEquation(1, 2, 3);
-
             bool result = a[1] == 2;
-
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void LinearEquationTestIndexer2()
         {
             LinearEquation a = new LinearEquation(1, 2, 3);
-
             bool result = a[2] == 3;
-
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void LinearEquationTestIndexer3()
         {
             LinearEquation a = new LinearEquation(1, 2, 3);
-
             bool result = a[0] == 1;
-
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void LinearEquationTestIndexerException()
         {
             LinearEquation a = new LinearEquation(1, 2);
-
             Exception result = Assert.ThrowsException<ArgumentOutOfRangeException>(() => a[-1]);
-
             Assert.IsInstanceOfType(result, typeof(ArgumentOutOfRangeException));
-
         }
         [TestMethod]
         public void LinearEquationTestIndexerException2()
         {
             LinearEquation a = new LinearEquation(1, 2);
-
             Exception result = Assert.ThrowsException<ArgumentOutOfRangeException>(() => a[2]);
-
             Assert.IsInstanceOfType(result, typeof(ArgumentOutOfRangeException));
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqPlusFloat()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            float b = 5.4F;
+            LinearEquation result = a + b;
+            Assert.AreEqual(new LinearEquation(1, 2, 8.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqPlusFloat2()
+        {
+            LinearEquation a = new LinearEquation(1, 2);
+            float b = 5.4F;
+            LinearEquation result = a + b;
+            Assert.AreEqual(new LinearEquation(1, 7.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqPlusFloat3()
+        {
+            LinearEquation a = new LinearEquation(1, 2);
+            float b = 1F;
+            LinearEquation result = a + b;
+            Assert.AreEqual(new LinearEquation(1, 3F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqPlusFloat4()
+        {
+            LinearEquation a = new LinearEquation(1, 2);
+            float b = -1F;
+            LinearEquation result = a + b;
+            Assert.AreEqual(new LinearEquation(1, 1F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            float b = 5.4F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, 2, -2.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat2()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            float b = -5.4F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, 2, 8.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat3()
+        {
+            LinearEquation a = new LinearEquation(1, 2);
+            float b = 5.4F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, -3.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat4()
+        {
+            LinearEquation a = new LinearEquation(1, 3);
+            float b = -5.4F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, 8.4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat5()
+        {
+            LinearEquation a = new LinearEquation(1, 3);
+            float b = -1F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, 4F), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOpeartorEqMinusFloat6()
+        {
+            LinearEquation a = new LinearEquation(1, 3);
+            float b = 1F;
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(1, 2F), result);
+        }
+
+        [TestMethod]
+        public void LinearEquationTestOperatorEqPlusEq()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            LinearEquation b = new LinearEquation(4, 5);
+            LinearEquation result = a + b;
+            Assert.AreEqual(new LinearEquation(5, 2, 8), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOperatorEqMinusEq()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            LinearEquation b = new LinearEquation(4, 5);
+            LinearEquation result = a - b;
+            Assert.AreEqual(new LinearEquation(-3, 2, -2), result);
+        }
+        [TestMethod]
+        public void LinearEquationTestOperatorTrue()
+        {
+            LinearEquation a = new LinearEquation(0, 0, 0, 1);
+            bool ans = false;
+            if (a)
+                ans = true;
+            else
+                ans = false;
+            Assert.IsFalse(ans);
+        }
+
+        [TestMethod]
+        public void LinearEquationTestSolve()
+        {
+            LinearEquation a = new LinearEquation(2, 0, 5);
+            float ans = 0;
+            a.Solve(out ans);
+            float trueans = (0F - 5F) / 2F;
+            bool t = ans.CompareTo(trueans) == 0;
+            Assert.IsTrue(t);
+        }
+
+        [TestMethod]
+        public void LinearEquationTestToString()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3, 4, 5);
+            bool ans = a.ToString() == "1,2,3,4,5";
+            Assert.IsTrue(ans);
+        }
+        [TestMethod]
+        public void LinearEquationTestInitSame()
+        {
+            LinearEquation a = new LinearEquation(true, 3, 5);
+            LinearEquation res = new LinearEquation(5, 5, 5);
+            bool ans = (a[0] == res[0]) &&
+                       (a[1] == res[1]) &&
+                       (a[2] == res[2]);
+            Assert.IsTrue(ans);
+        }
+        [TestMethod]
+        public void LinearEquationTestInitRandom()
+        {
+            LinearEquation a = new LinearEquation(true, 3);
+            bool ans = a != null;
+            Assert.IsTrue(ans);
+        }
+        [TestMethod]
+        public void LinearEquationTestOperatirEqMultFloat()
+        {
+            LinearEquation a = new LinearEquation(1, 2, 3);
+            a = a * 2F;
+            Assert.AreEqual(4F, a[1]);
+        }
+
+        [TestMethod]
+        public void LinearEquationTestOperatorMinus()
+        {
+            LinearEquation a = new LinearEquation(1, 0, 3);
+            a = -a;
+            Assert.AreEqual(-1, a[0]);
+        }
+        [TestMethod]
+        public void LinearEquationTestOperatorMinus1()
+        {
+            LinearEquation a = new LinearEquation(1, 0, 3);
+            a = -a;
+            Assert.AreEqual(0, a[1]);
+        }
+
+        [TestMethod]
+        public void LinearEquationTestToDouble()
+        {
+            LinearEquation a = new LinearEquation(9, 8);
+
+            Assert.AreEqual(typeof(List<double>), a.ToDouble().GetType());
         }
     }
 }
